13a28e2c50cd41520cbad48e20d3da85882cbee8
Chia <chiaxiong19@gmail.com>
2023-08-07 23:53:20 -0500

    Lab 9 Commit

--- app/src/main/java/com/ics342/labs/NumbersRepository.kt
+++ app/src/main/java/com/ics342/labs/NumbersRepository.kt
@@ -1,5 +1,13 @@
 package com.ics342.labs
 
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
 class NumbersRepository(private val database: Database, private val api: Api) {
 
     fun fetchNumbers(): List<Number> {
@@ -16,4 +24,26 @@ class NumbersRepository(private val database: Database, private val api: Api) {
     fun getNumber(id: String): Number? {
         return database.getNumber(id) ?: api.getNumber(id)
     }
+
+    @Test
+    fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
+
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val number = Number(UUID.randomUUID().toString(), Random.nextInt())
+
+        every { database.getAllNumbers() } returns listOf()
+        every { api.getNumbers() } returns listOf(number)
+        every { database.storeNumbers(listOf(number)) } just Runs
+
+        val numbersRepository = NumbersRepository(database, api)
+        val result = numbersRepository.fetchNumbers()
+
+        assertEquals(result, listOf(number))
+
+        verify { database.getAllNumbers() }
+        verify { api.getNumbers() }
+
+    }
+}
 }
